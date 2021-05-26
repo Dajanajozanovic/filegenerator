64669e2d642360e3f3900c91c0de7aaee2a18b89
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-05-26 11:29:20 +0200

    create writeFile.js and export function writeFile

--- index.js
+++ index.js
@@ -2,9 +2,4 @@ const fs = require('fs')
 
 const functionName = process.argv[2] ?? 'myFunction'
 
-fs.writeFileSync(
-  `./${functionName}.js`,
-  `function ${functionName}() {
-
-    }`
-)
+writeFile(functionName)
